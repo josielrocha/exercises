3eb3dc4eeed6c203c131585233321301e9d09be2
Josiel Rocha <josiel.rocha@gmail.com>
2015-04-04 21:58:55 -0300

    Atividade 02 da unidade 01

--- praticas-de-programacao/Unit01.java
+++ praticas-de-programacao/Unit01.java
@@ -5,6 +5,7 @@ public class Unit01 {
 
         // Run exercises
         exerciseOne();
+        exerciseTwo();
     }
 
     public static void exerciseOne() {
@@ -13,6 +14,10 @@ public class Unit01 {
         int greatherValue = 0;
         int greatherValueOrder = 0;
         int number = 0;
+
+        System.out.println("===================================================");
+        System.out.println("Exercício 01");
+        System.out.println("===================================================");
 
         for (int i=0; i<numbers.length; i++) {
             System.out.print("Digite um número inteiro: ");
@@ -30,7 +35,41 @@ public class Unit01 {
         System.out.println("Foram lidos " + numbers.length + " números");
         System.out.println("O maior valor lido foi: " + greatherValue);
         System.out.println("O maior valor foi informado ao digitar o " + greatherValueOrder + "º número");
-        System.out.println("===================================================");
     }
+
+    public static void exerciseTwo() {
+        Scanner input = new Scanner(System.in);
+        double weights[] = new double[35];
+        int ableFighters = 0;
+        double sum = 0.0;
+        double weight, average;
+
+        System.out.println("===================================================");
+        System.out.println("Exercício 02");
+        System.out.println("===================================================");
+
+        for (int i=0; i<weights.length; i++) {
+            System.out.print("Informe o peso do " + (i + 1) + "º lutador: ");
+            weight = input.nextDouble();
+
+            sum += weight;
+            weights[i] = weight;
+        }
+
+        // average
+        average = sum / weights.length;
+
+        for (int i=0; i<weights.length; i++) {
+            weight = weights[i];
+
+            if (weight >= average) {
+                ableFighters++;
+            }
+        }
+
+        System.out.println("===================================================");
+        System.out.println("Foram lidos os pesos de " + weights.length + " lutadores");
+        System.out.println("O peso médio obtido é: " + average);
+        System.out.println(ableFighters + " lutadores possuem o peso maior ou igual a média");
 }
 
